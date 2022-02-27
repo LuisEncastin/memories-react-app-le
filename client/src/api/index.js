45922212f8ef6fac4fe69cfbd68b99b8bc946a49
LuisEncastin <luisencastin@gmail>
2022-02-27 01:24:19 -0600

    Cambio en la url que conecta con la api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://memories-mern-app-luisencastin.herokuapp.com/';
+const url = 'https://memories-mern-app-luisencastin.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
